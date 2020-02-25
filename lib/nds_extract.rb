b7dc40b252f03d0150316d627a02c4d014b646d8
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 04:50:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,7 +50,8 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name method
 
   result = []
-  result << movie_with_director_name(name,movies_collection)
+  tempHash = {}
+  result << tempHash[name] = movie_with_director_name(name,movies_collection)
   result
 end
 
