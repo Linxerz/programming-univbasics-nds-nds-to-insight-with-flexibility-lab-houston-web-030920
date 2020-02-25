1cecf4839a5ddd075a379b2c471aa237eaaf38e5
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 05:24:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -51,7 +51,7 @@ def movies_with_director_key(name, movies_collection)
 result = []
 director_hash = {}
 pp movies_collection
-director_hash[movie_with_director_name(name, movies_collection[0])[:director_name]] = movies_collection
+director_hash[movie_with_director_name(name, movies_collection[0])[:director_name]] = name
 result << director_hash
 pp result
 end
