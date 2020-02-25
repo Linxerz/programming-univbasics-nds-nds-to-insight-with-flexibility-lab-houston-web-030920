41331e366f46f260e24365d23b1a4275344212e2
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 05:32:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,6 +50,7 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name method
   movies_collection.each do |movie|
     movie[:director_name] = movie_with_director_name(name,movie)[:director_name]
+  end
 end
 
 
