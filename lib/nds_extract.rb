b5de739e2d72a3ce1d2f34c40c7a985338308704
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 05:47:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -55,11 +55,18 @@ def gross_per_studio(collection)
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
   pp collection
-  total = 0
+  result = {}
+
   collection.each do |movie|
-    total += movie[:worldwide_gross]
+    total = 0
+    collection.each do |studio|
+      if movie[:studio] == studio[:studio]
+        total += studio[:worldwide_gross]
+      end
+    end
+    result [movie[:studio]] = total
   end
-  total
+  pp result
 end
 
 def movies_with_directors_set(source)
