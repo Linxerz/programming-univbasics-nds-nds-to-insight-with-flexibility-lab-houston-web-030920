19bee7cdb113cb557931aa0db7f5dff76158ec83
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 05:52:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -69,8 +69,13 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-
+  result = []
   pp source
+  source.each do |director|
+    director[:movies].each do |movie|
+      movie[:director_name] = director[:name]
+    end
+
 
 end
 
