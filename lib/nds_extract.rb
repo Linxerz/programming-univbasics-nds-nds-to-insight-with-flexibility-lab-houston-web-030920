95505db32ff2e7645dd9ee827eb1df9b6e5e7b02
Johnathan Pruitt <pruittj2000@yahoo.com>
2020-02-25 05:34:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -54,6 +54,8 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
+
+  pp collection
 end
 
 def movies_with_directors_set(source)
